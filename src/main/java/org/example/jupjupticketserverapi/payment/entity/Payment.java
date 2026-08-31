1d1c86525de63f2541a2bac9fcb38d06e017ed2c
heeheepark <pjh9641@gmail.com>
2026-08-31 05:59:15 +0200

    feat: payment 엔티티 수정

--- src/main/java/org/example/jupjupticketserverapi/payment/entity/Payment.java
+++ src/main/java/org/example/jupjupticketserverapi/payment/entity/Payment.java
@@ -36,6 +36,8 @@ public class Payment {
 
     private LocalDateTime paidAt;
 
+    private LocalDateTime createdAt;
+
     public Payment(
             Reservation reservation,
             BigDecimal amount,
@@ -45,6 +47,7 @@ public class Payment {
         this.amount = amount;
         this.paymentMethod = paymentMethod;
         this.status = PaymentStatus.PENDING;
+        this.createdAt = LocalDateTime.now();
     }
 
     public void complete() {
