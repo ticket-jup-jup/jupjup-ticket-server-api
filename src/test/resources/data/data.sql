59113d92b8e0e757f691b2d43fcb6ce6bf8fd27d
heeheepark <pjh9641@gmail.com>
2026-09-03 21:39:04 +0200

    fix: ci 오류 수정

---
+++ src/test/resources/data/data.sql
@@ -0,0 +1,306 @@
+#
+사용자 더미데이터
+INSERT INTO users (
+    email, name, password, deleted_at, created_at, updated_at
+) VALUES
+      ('dookie289@gmail.com', '최형석', '$2y$04$I5eHpoVTNGKXvQOrvwz8we5x24A0pp1F34uEdYMp66UgjLdxWHpdi', NULL, NOW(), NOW()),
+      ('treeoark1@gmail.com', '박준용', '$2y$04$88yytUG6Db4U9/F2KBKlq.7GPX0mcC0i6AaNK36.d/q6eLpNaxEyG', NULL, NOW(), NOW()),
+      ('mirumew@gmail.com', '김예슬', '$2y$04$m1HIFpXe9J5VMmXDqnIPPObnB7QPY7/XqZPzbrU7jSTVLvLc13rY2', NULL, NOW(), NOW()),
+      ('dummy001@example.com', '김민준', '$2y$04$OumAec.FIGd6S3gqbBd2g.1WY1dzyx8iu.PMZkVOELq2IaUk8GdUq', NULL, NOW(), NOW()),
+      ('dummy002@example.com', '이서연', '$2y$04$bimFkK.kJbhMBtAk08BL0u2lJbuI3/UOVOK.SdBTbi0Rm5xPcvgca', NULL, NOW(), NOW()),
+      ('dummy003@example.com', '박지훈', '$2y$04$mX/jHPJWu9OAd3GWcYb07.kn7Au/YvAfPosIQjGGVQ56JA4n06lfW', NULL, NOW(), NOW()),
+      ('dummy004@example.com', '최수진', '$2y$04$jxVg/VRnQbt6OsPO0TrEDumgbKUvUhfLKi24GVEy/VdJg6JdhZIy2', NULL, NOW(), NOW()),
+      ('dummy005@example.com', '정혜진', '$2y$04$teALwDaXyps8jou4HRsDq.H9f7fX0H5YQD.TQ43PRkuzCuDU7GPLG', NULL, NOW(), NOW()),
+      ('dummy006@example.com', '강동현', '$2y$04$.1R/3KwhSHs/2P3DsVKJm.0uqNP0vK805r8/ZB1jdkwIGOHUKn8fi', NULL, NOW(), NOW()),
+      ('dummy007@example.com', '윤지원', '$2y$04$wsgQoVBSg.kE0FkECVoqwOtu.UHH3YdjIs5ojlAYOuELjCkmZ3e5G', NULL, NOW(), NOW()),
+      ('dummy008@example.com', '한준서', '$2y$04$89lvqDFVZSLimAbUwxRNTOojFQzH0K5L6.vJx7YaGxUASAiD2CVem', NULL, NOW(), NOW()),
+      ('dummy009@example.com', '신유나', '$2y$04$.s1OmvozLaUYD6aOCn6aT.MF9EliG2SUyhfggU3Su9venLZFsrUo.', NULL, NOW(), NOW()),
+      ('dummy010@example.com', '김도윤', '$2y$04$kFQ3XIM/NpdofqW/tfn3/.unlq7l59cmDK9ZVUJnwutBTZf4byy0S', NULL, NOW(), NOW()),
+      ('dummy011@example.com', '이하은', '$2y$04$QiDa.d/qezVKPfH8jBtrwevKog3/hcy2N.eReqejYKm6IDfyKDE0y', NULL, NOW(), NOW()),
+      ('dummy012@example.com', '박서준', '$2y$04$SGukvXcDfmwUwPRMyOzChOtNmJw06P8pciohmtSp3hbvmZ94moxpK', NULL, NOW(), NOW()),
+      ('dummy013@example.com', '최지우', '$2y$04$QO79eYXPO0GXliA0Y5mXw.7vHkIqvyxLyDwevGelg84dH02dOFEea', NULL, NOW(), NOW()),
+      ('dummy014@example.com', '정우진', '$2y$04$qkD2l0U8cfH0ocxD2e0qsOuRZZFSpyEnyVrwXJ85VzXHATVAxhtlq', NULL, NOW(), NOW()),
+      ('dummy015@example.com', '강서윤', '$2y$04$bOaTFTgaRFQyA1pbKg.sQ.AdL/OaiDnb3H/AaPUc0Vln8g3Lnk6IW', NULL, NOW(), NOW()),
+      ('dummy016@example.com', '윤서준', '$2y$04$4i9uOKjaX28kjgoiXve4qeQ26QtjJMyggkTGNMI.f8zwa71vnsACu', NULL, NOW(), NOW()),
+      ('dummy017@example.com', '한지민', '$2y$04$nvC7Kc50mMoMx0IPVSroOuU6TIfHdOb9Y6TOGidgbzR6AZFHtLUW6', NULL, NOW(), NOW()),
+      ('dummy018@example.com', '신민재', '$2y$04$qTq1jfhU5S/2ltnhyoB4NuOjFstQ8KvnybOC8MpMlAl8USZ7XgIBi', NULL, NOW(), NOW()),
+      ('dummy019@example.com', '김하늘', '$2y$04$4K3ftuy4CKAb3yw3uc.hgOne32yl0Nbwe9RY7Rb8EmVE6V8cR.7iG', NULL, NOW(), NOW()),
+      ('dummy020@example.com', '이준혁', '$2y$04$YtEMdPdlTXJk4VQgZD9Wwe1B2a6zXj9Z.UASXvI628eKFZY8Pf2nG', NULL, NOW(), NOW()),
+      ('dummy021@example.com', '박수빈', '$2y$04$iJHW/aS8Ev4Qpt3KN3xVkuIRXH8OfQ74wokOhwoYx.96RCP28ukVK', NULL, NOW(), NOW()),
+      ('dummy022@example.com', '최현우', '$2y$04$kDfXVt41RR8UCk6iAEYmoOd6uaLh7uA.OKX6DYxeW/95DydxxnOi.', NULL, NOW(), NOW()),
+      ('dummy023@example.com', '정다은', '$2y$04$ZqVaWEIqadq3SVkAv/Wxr.jx/vmrA9Uye2KY4eN7F81zOi22vLrvW', NULL, NOW(), NOW()),
+      ('dummy024@example.com', '강예준', '$2y$04$B4dVqvS.CYHfRMA0fc85dOMTuHBff2WeJ3bIMKohJTrx0eZ36Xeti', NULL, NOW(), NOW()),
+      ('dummy025@example.com', '윤채원', '$2y$04$VXNIH4Evz2xqYfCCHYrAjegJuqR7C5eOP5FqEQl8njvEWdwK.09EG', NULL, NOW(), NOW()),
+      ('dummy026@example.com', '한승우', '$2y$04$ACQQhsDZrMT.sKC2sCyA4eDUy.QQ/FFtDVtDHjozXFfulzgd7XR8m', NULL, NOW(), NOW()),
+      ('dummy027@example.com', '신지아', '$2y$04$i9K19ccSntNyTKBA6RH2Ru6ianpQAJ54qKjW19uZTwgWARNKGU3OK', NULL, NOW(), NOW()),
+      ('dummy028@example.com', '김지훈', '$2y$04$9JF956LVlMAqqiXqVDvueeOfy6Rjdw36wF2UHa6fViOzBbkSC.8ge', NULL, NOW(), NOW()),
+      ('dummy029@example.com', '이채은', '$2y$04$ahIHkcY1TIfthu9P0c2a9uKCvG5ryNrQlx9PZ3Fjmg0bygXFrOdcy', NULL, NOW(), NOW()),
+      ('dummy030@example.com', '박건우', '$2y$04$g9TXz98rpt3TpVel8dWw.OKdg2/8qNPlw461kc/hVgc.woq7fCiKe', NULL, NOW(), NOW()),
+      ('dummy031@example.com', '최유진', '$2y$04$CG0UWg1mWzUIVBnODVQn3uBxqjjyhrAEaX0LF9e8dWh0zyIN4xkl.', NULL, NOW(), NOW()),
+      ('dummy032@example.com', '정민수', '$2y$04$XlcWD6sw6rSMFOzHlg1eU.3YfdPXd99CBJk0QrB7JV1UAvgtfHmV2', NULL, NOW(), NOW()),
+      ('dummy033@example.com', '강나연', '$2y$04$Axh3zQVTkuSFyjPbqWyb6.u1hJNqPbKsSp1CQsddmp/zCLedyHcHu', NULL, NOW(), NOW()),
+      ('dummy034@example.com', '윤지호', '$2y$04$S8jcRjUNtYwF94jkuIGBAecGYn5M3cli53HjnSKMFgD..tHQdBJbm', NULL, NOW(), NOW()),
+      ('dummy035@example.com', '한소희', '$2y$04$JpE1m7ci7ggVC.n82DFGEupCtx5Ye/nxQ1bKKcq4Zxr7oMRwX.0t.', NULL, NOW(), NOW()),
+      ('dummy036@example.com', '신현준', '$2y$04$6cfgUEWCSR/HuUuJa8tGhuDQkiEo3Yb54p20yFTuQocBt6LU0aHYS', NULL, NOW(), NOW()),
+      ('dummy037@example.com', '김서현', '$2y$04$8Qd8TM2Ed2zSyLQEzAs87ewB5BVqiBNiPkhxWlEmX/PDIB7yjAur6', NULL, NOW(), NOW()),
+      ('dummy038@example.com', '이도현', '$2y$04$vsapduRAC2q99QbClf1TgOqnfUYG2x6an6eJ.ghKwzKe5mkgwDd4y', NULL, NOW(), NOW()),
+      ('dummy039@example.com', '박예은', '$2y$04$R1JlY5M.D7ZBNiJa2lbT1.q4zMtKJVT0tJgH8N0OuSh4DIINAMbYe', NULL, NOW(), NOW()),
+      ('dummy040@example.com', '최준영', '$2y$04$OLNJd.kCewW9aDd7a3qHMecLwgK80ee1ONJ29EfC0lqHzW1EfpWaC', NULL, NOW(), NOW()),
+      ('dummy041@example.com', '정유나', '$2y$04$.DkXs/fgeka4amcXjDDTQeHsOT7cMtQaC8BwUstVeTcogj2KCr8j2', NULL, NOW(), NOW()),
+      ('dummy042@example.com', '강민석', '$2y$04$qWMI5IBlsOS2mECd9H4YLu/B/9wt/iL7yrMPiV0ibSnB8w64I0wLG', NULL, NOW(), NOW()),
+      ('dummy043@example.com', '윤하린', '$2y$04$JDad49jvx1gKIKvaN4b97OvqGUR9.ZpugYy/i7CRddqdKpNAHrl/e', NULL, NOW(), NOW()),
+      ('dummy044@example.com', '한태양', '$2y$04$JCbUKSCQrAfGfVteliCB5efpkDSV6xMNWZE.QMDV8IlFm7F7gmony', NULL, NOW(), NOW()),
+      ('dummy045@example.com', '신서연', '$2y$04$9v5RB/d2Zp12FcqpmvKhmOcM86i6gnIj3qRXOCmLDPJRujhp7cgCe', NULL, NOW(), NOW()),
+      ('dummy046@example.com', '김현수', '$2y$04$q6CUIzsVj6hJJqM2P0bIa.9tCKn5e9XIGswDkyUpGt9x3UaOSpugS', NULL, NOW(), NOW()),
+      ('dummy047@example.com', '이민재', '$2y$04$QsEuhzyXEg5wx4MMaLonluAfMjBqJD/mD2RjmgfZReq2DzSQ0zvie', NULL, NOW(), NOW()),
+      ('dummy048@example.com', '박지우', '$2y$04$28LnW8beh2aq/dj7F12ElOoTluUPMAvr.4uxaX/NXpp0H/lKz3oQy', NULL, NOW(), NOW()),
+      ('dummy049@example.com', '최서윤', '$2y$04$isWpz.LxEWha5PntLhppLePHKmwPBLYzjNkXQAfEVX4fWf0L0nlBm', NULL, NOW(), NOW()),
+      ('dummy050@example.com', '정현우', '$2y$04$G1YkWXrJZiAP3HCUPr/oBeGAPrDWaTvWGxC/QpGZNKytQctHi5IX.', NULL, NOW(), NOW()),
+      ('dummy051@example.com', '강지훈', '$2y$04$T0iZL/2pqfjI92YjE4Ks7eJ4OwbLksCawF0WcTeFwiXqaqHBxMqIW', NULL, NOW(), NOW()),
+      ('dummy052@example.com', '윤수빈', '$2y$04$FPjq0KJPZb86TW4R5jPdd.dlLgbdRwDHcVozYnMfkw1nFymQc9fVy', NULL, NOW(), NOW()),
+      ('dummy053@example.com', '한도윤', '$2y$04$n4I3LKYUvn7kt4yq3RFO1.XjJNKXzTJEKB5/GWT8g/O8atjgpCMBe', NULL, NOW(), NOW()),
+      ('dummy054@example.com', '신예린', '$2y$04$YoAUw6s/AW7GPm0z2yUyGeWxyfgm3g.XnNrxQ2oWRap7DF07bxWxy', NULL, NOW(), NOW()),
+      ('dummy055@example.com', '김재현', '$2y$04$jtktRRelXHhZj2jIMmqB0Oo4cooj5jgmmZs1lQPOuqeesOm93dmgK', NULL, NOW(), NOW()),
+      ('dummy056@example.com', '이수민', '$2y$04$l0duMufhHy/A/eWUK0GpiOISxN4ogc5NkCBmqCJOozyvl9qw4SaKG', NULL, NOW(), NOW()),
+      ('dummy057@example.com', '박현우', '$2y$04$xoFPwL1LAKqJSIn8ryddiOK10NCCPrl9TlMjHPfkhlD3oNKykoWga', NULL, NOW(), NOW()),
+      ('dummy058@example.com', '최민준', '$2y$04$ZHoIGWpEH9YHzEO8q4T9GOiAgl31xWGuj4bcTx/BRoDkY2LO26eoS', NULL, NOW(), NOW()),
+      ('dummy059@example.com', '정서현', '$2y$04$5KVYz9nXYG4k4fB6ghjEvuNz8DRX4fepdU/m8UODaRFAd70i4AXz6', NULL, NOW(), NOW()),
+      ('dummy060@example.com', '강준호', '$2y$04$n8y0JoQPZGIZBabXLPebnezPnHLZZeBGC3NUVeimHsEIe38Twwoxy', NULL, NOW(), NOW()),
+      ('dummy061@example.com', '윤지민', '$2y$04$kHmaquBSHB9.OhyX8kNvbO5sVrV6PsKO0mBWj6EmetRugYhnYNGXq', NULL, NOW(), NOW()),
+      ('dummy062@example.com', '한예준', '$2y$04$DsXB9MmRDPWPl6lVrgS4QOaIPcItQ72luWQeh.AEoqZfHl93pT3XG', NULL, NOW(), NOW()),
+      ('dummy063@example.com', '신지훈', '$2y$04$dNeTHXJ9hsbQ/CfW2RkgHeX/EUaK6BDzeHpnuojLoxOxyjTAnzJEC', NULL, NOW(), NOW()),
+      ('dummy064@example.com', '김도현', '$2y$04$ZsIHuACZP5/JueAP0P8bjuN.yd.X4fArmifS7GPHosjZumuBQsDyq', NULL, NOW(), NOW()),
+      ('dummy065@example.com', '이유진', '$2y$04$4FO3aUKDsNxTYIbl3t9MyOhdNkAi5UcEanmEzCyIXAfbj7wi8Kk9K', NULL, NOW(), NOW()),
+      ('dummy066@example.com', '박성민', '$2y$04$TpDI6qpFjTlwoBBtXiG9CeYNw8EkacRqOsbkSwxEQNfUKmw0aBphC', NULL, NOW(), NOW()),
+      ('dummy067@example.com', '최지훈', '$2y$04$vgGPNizBgd2YjA5W.Vc6J.XbWsFeNETLecHYYWezByMbXSEYTJNga', NULL, NOW(), NOW()),
+      ('dummy068@example.com', '정하은', '$2y$04$oxnC2HOYqRBGRYtWfvrQbOaIXSjEA7rtxC.mIo/Kshzpc0POIS01W', NULL, NOW(), NOW()),
+      ('dummy069@example.com', '강현우', '$2y$04$1HSgGPe1JaST/lm6xWxIAuDC3KbR6iN1NGa9X0XepZrYK9X29i/Ge', NULL, NOW(), NOW()),
+      ('dummy070@example.com', '윤민서', '$2y$04$UqxwvXATAW5iFbzfKAc4lu/3LIXJ5S6otyXAtSgzkLHh51BGWQUKS', NULL, NOW(), NOW()),
+      ('dummy071@example.com', '한지훈', '$2y$04$Y6yMrPF6h46x74cMAlwcieSrtira4KwVnDXf77SGPVS7Ngf7QMvoW', NULL, NOW(), NOW()),
+      ('dummy072@example.com', '신다은', '$2y$04$SAApKyCuQLh8xnrsBuCNkuMUDjtqZMZvx8EE7Ckk7GNtVQCuizEEK', NULL, NOW(), NOW()),
+      ('dummy073@example.com', '김우진', '$2y$04$6qO4Qo.VtFE3hGdr3ITfxeWW85pFoyauTt3Qa4dLeQ1sWbvPZxY12', NULL, NOW(), NOW()),
+      ('dummy074@example.com', '이서준', '$2y$04$kcAc.nM47gQSYHf6ryFB8.RsX6w8raCVF2eZI8MA/i1Bqk/hJxaM2', NULL, NOW(), NOW()),
+      ('dummy075@example.com', '박민지', '$2y$04$W87UbM3V0rjuYovHbChUxe2kGxOzXQmBcyb2l4H53nS0.8.hniUjC', NULL, NOW(), NOW()),
+      ('dummy076@example.com', '최도윤', '$2y$04$GmZWxdDFGW3JLUccnDcqRexpX3FRFSa2KJUhIDUnkMjuwH0FPfqz6', NULL, NOW(), NOW()),
+      ('dummy077@example.com', '정지훈', '$2y$04$mKTx9p/dnqlV.NQBJ0XdVuAwOaJJhDY1g/PWa.I2QKFSSVsBm4mG6', NULL, NOW(), NOW()),
+      ('dummy078@example.com', '강서준', '$2y$04$gM1/JAiuZPnkCKOe.omEJ.G4wGfu8bKhW1BdEPMOKZ36YlqXFxrfa', NULL, NOW(), NOW()),
+      ('dummy079@example.com', '윤예은', '$2y$04$wS8aycm6mCEWfDZVmYDwBeTcZcNdC/.GuLfnINun6pIBS3tql.uyi', NULL, NOW(), NOW()),
+      ('dummy080@example.com', '한민재', '$2y$04$UI7P.we.7fa0kiRerBk3GuGOQU7S5YWpf1SwtPTzZrvQUEOVHEBIC', NULL, NOW(), NOW()),
+      ('dummy081@example.com', '신준호', '$2y$04$ptiIdOO3NJW7pYnJh5tcUOt0FxnV/GaJYy9YCOUmDc.ZrrHLjrvk.', NULL, NOW(), NOW()),
+      ('dummy082@example.com', '김수현', '$2y$04$LeTbeQj3qZ8Pj9j6g3GzVu7DGfiLGFmxpfHz1VdCCIxW4.WscL.Me', NULL, NOW(), NOW()),
+      ('dummy083@example.com', '이지훈', '$2y$04$BGcTooCVug2e7jZZ8WCSlufgdx9/Lk45PWXVuO1Rjfe6QturWS0Xm', NULL, NOW(), NOW()),
+      ('dummy084@example.com', '박준서', '$2y$04$lmf975Vj1AWPz/FSAh6ygeikbaAb2Zf9WhyGh9xMnarM5oEADoyXi', NULL, NOW(), NOW()),
+      ('dummy085@example.com', '최하은', '$2y$04$BJT1HD233QsecrB2Hhaq4ehamGjopo6n/IfZ2y/5wDAvJqFgsR5kO', NULL, NOW(), NOW()),
+      ('dummy086@example.com', '정민재', '$2y$04$.wiyxm1ziMAzZ3O7yKKZKe0RA8SKusCqvZplmJwIoeJxfVPWXRDwG', NULL, NOW(), NOW()),
+      ('dummy087@example.com', '강유진', '$2y$04$ZADfPbGOvrVlNucn0kWLVOBK3ov4GpUq8WmM/HyVbKSyiPsYJ.0kS', NULL, NOW(), NOW()),
+      ('dummy088@example.com', '윤현우', '$2y$04$MhrwylZRYdjSBjnEHpQa1uGnwcTWL3/eSO0D2RKBrWZtYDuaBuuNS', NULL, NOW(), NOW()),
+      ('dummy089@example.com', '한서윤', '$2y$04$rykPImvhBryDoRVCJb6PYe4KeJuJJZ9z6UCvhgb.fobImxZGAc9KG', NULL, NOW(), NOW()),
+      ('dummy090@example.com', '신도윤', '$2y$04$fHZCjhXMfaoJNvQtEMTZ1.4hzdKp0xtR7T3Zcdpfx5nnPIBeeuwAu', NULL, NOW(), NOW()),
+      ('dummy091@example.com', '김준영', '$2y$04$Qd74dPbh4ci8nvqP/.Y/.uSd5.NPhb7nV4Hab85SyTTXs6fN2cxym', NULL, NOW(), NOW()),
+      ('dummy092@example.com', '이현지', '$2y$04$07cABgitlIQ711AnssKtBus5LMB.sjmJh47PTnbhr6afwZTLtum9G', NULL, NOW(), NOW()),
+      ('dummy093@example.com', '박도현', '$2y$04$1eIg8ZBA7kctlpx1Qv/KrOBMH/6kDeEuQlG8iJZjVgfVb5zcOQpyW', NULL, NOW(), NOW()),
+      ('dummy094@example.com', '최민석', '$2y$04$mpaWBlrX3WnvX01PvAIBf.nmz7DgZLXt6RyuF1RxHQyDI4HIXERQu', NULL, NOW(), NOW()),
+      ('dummy095@example.com', '정예린', '$2y$04$Bhx6S0V1q86f1vIImBqQmObpHn2cANuzD4vNizVFiv0VBuL8.7K2K', NULL, NOW(), NOW()),
+      ('dummy096@example.com', '강지우', '$2y$04$hsjj3Pjh0XsblL.Cx9xLAOddCBTbLiifDyGuAqL.vNXlO6BmzY8V.', NULL, NOW(), NOW()),
+      ('dummy097@example.com', '윤준호', '$2y$04$GemVn22r4v/3Ir1HZqssTeeLiVc1Kx57LQ.Cdvxz8bVHzcib7zrMS', NULL, NOW(), NOW());
+
+#
+공연 더미데이터
+INSERT INTO programs (
+    name,
+    description,
+    type,
+    created_at,
+    updated_at,
+    deleted_at
+) VALUES
+      (
+          '2026 JUPJUP 콘서트',
+          '줍줍 테스트용 콘서트',
+          'CONCERT',
+          NOW(),
+          NOW(),
+          NULL
+      ),
+      (
+          '2026 JUPJUP 뮤지컬',
+          '줍줍 테스트용 뮤지컬',
+          'MUSICAL',
+          NOW(),
+          NOW(),
+          NULL
+      ),
+      (
+          '서울 부산 특별열차',
+          '줍줍 테스트용 열차',
+          'TRAIN',
+          NOW(),
+          NOW(),
+          NULL
+      );
+
+#
+회차 데이터
+INSERT INTO performances (
+    start_at,
+    end_at,
+    status,
+    venue,
+    program_id,
+    created_at,
+    updated_at
+) VALUES
+      (
+          '2026-09-10 19:00:00',
+          '2026-09-10 21:30:00',
+          'UPCOMING',
+          '서울 올림픽공원',
+          1,
+          NOW(),
+          NOW()
+      ),
+      (
+          '2026-09-11 19:00:00',
+          '2026-09-11 21:30:00',
+          'UPCOMING',
+          '서울 올림픽공원',
+          1,
+          NOW(),
+          NOW()
+      ),
+      (
+          '2026-09-20 14:00:00',
+          '2026-09-20 17:00:00',
+          'UPCOMING',
+          '블루스퀘어',
+          2,
+          NOW(),
+          NOW()
+      ),
+      (
+          '2026-09-20 19:00:00',
+          '2026-09-20 22:00:00',
+          'UPCOMING',
+          '블루스퀘어',
+          2,
+          NOW(),
+          NOW()
+      ),
+      (
+          '2026-10-01 09:00:00',
+          '2026-10-01 12:00:00',
+          'UPCOMING',
+          '서울역',
+          3,
+          NOW(),
+          NOW()
+      ),
+      (
+          '2026-10-02 09:00:00',
+          '2026-10-02 12:00:00',
+          'UPCOMING',
+          '서울역',
+          3,
+          NOW(),
+          NOW()
+      );
+
+#
+좌석 더미데이터
+INSERT INTO seats (section, seat_row, seat_number)
+VALUES
+    ('A', 'A', 1),
+    ('A', 'A', 2),
+    ('A', 'A', 3),
+    ('A', 'A', 4),
+    ('A', 'A', 5),
+    ('B', 'B', 1),
+    ('B', 'B', 2),
+    ('B', 'B', 3),
+    ('B', 'B', 4),
+    ('B', 'B', 5);
+
+#
+티켓 더미데이터
+-- 티켓 더미데이터
+INSERT INTO tickets (
+    performance_id,
+    seat_id,
+    price,
+    created_at,
+    updated_at
+) VALUES
+      (1, 1, 50000, NOW(), NOW()),
+      (1, 2, 50000, NOW(), NOW()),
+      (1, 3, 50000, NOW(), NOW()),
+      (1, 4, 50000, NOW(), NOW()),
+      (1, 5, 50000, NOW(), NOW()),
+      (1, 6, 70000, NOW(), NOW()),
+      (1, 7, 70000, NOW(), NOW()),
+      (1, 8, 70000, NOW(), NOW()),
+      (1, 9, 70000, NOW(), NOW()),
+      (1, 10, 70000, NOW(), NOW());
+
+-- 공연 2
+INSERT INTO tickets (performance_id,
+                     seat_id,
+                     price,
+                     created_at,
+                     updated_at)
+SELECT 2,
+       id,
+       CASE
+           WHEN section = 'A' THEN 50000
+    ELSE 70000
+END
+,
+    NOW(),
+    NOW()
+FROM seats;
+
+-- 공연 3
+INSERT INTO tickets (performance_id,
+                     seat_id,
+                     price,
+                     created_at,
+                     updated_at)
+SELECT 3,
+       id,
+       CASE
+           WHEN section = 'A' THEN 60000
+    ELSE 80000
+END
+,
+    NOW(),
+    NOW()
+FROM seats;
+
+-- 공연 4
+INSERT INTO tickets (performance_id,
+                     seat_id,
+                     price,
+                     created_at,
+                     updated_at)
+SELECT 4,
+       id,
+       CASE
+           WHEN section = 'A' THEN 60000
+    ELSE 80000
+END
+,
+    NOW(),
+    NOW()
+FROM seats;
+
+-- 공연 5
+INSERT INTO tickets (performance_id,
+                     seat_id,
+                     price,
+                     created_at,
+                     updated_at)
+SELECT 5,
+       id,
+       45000,
+       NOW(),
+       NOW()
+FROM seats;
