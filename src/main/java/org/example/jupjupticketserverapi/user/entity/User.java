b5c971e7e05f3aa5ef9bb8fc6e34c5b346fd1e13
heeheepark <pjh9641@gmail.com>
2026-09-03 10:10:39 +0200

    fix: 사용자 이메일 unique 추가

--- src/main/java/org/example/jupjupticketserverapi/user/entity/User.java
+++ src/main/java/org/example/jupjupticketserverapi/user/entity/User.java
@@ -18,7 +18,7 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 255)
+    @Column(unique = true, nullable = false, length = 255)
     private String email;
 
     @Column(nullable = false, length = 100)
