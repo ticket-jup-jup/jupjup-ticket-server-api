5a310a15cf0e24108b7e88045aa0957f7250fcbd
heeheepark <pjh9641@gmail.com>
2026-09-15 19:15:38 +0200

    chore: Validation 유효성 검사 Response 추가

--- src/main/java/org/example/jupjupticketserverapi/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/jupjupticketserverapi/global/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package org.example.jupjupticketserverapi.global.exception;
 
 import org.example.jupjupticketserverapi.global.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,15 +11,32 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ServiceException.class)
-    public ResponseEntity<ApiResponse<Void>> handleServiceException(
-            ServiceException ex
-    ) {
+    public ResponseEntity<ApiResponse<Void>> handleServiceException(ServiceException ex) {
         return ResponseEntity
                 .status(ex.getStatus())
                 .body(
                         ApiResponse.error(
                                 ex.getCode(),
                                 ex.getMessage()
+                        )
+                );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(error -> error.getDefaultMessage())
+                .orElse("잘못된 요청입니다.");
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        ApiResponse.error(
+                                "INVALID_REQUEST",
+                                message
                         )
                 );
     }
