d3f055ffcbb9eb531c42f957da9c53cc22fa81bf
heeheepark <pjh9641@gmail.com>
2026-09-15 22:37:36 +0200

    fix: 확정된 예약 취소 시 결제 데이터 삭제 로직 추가

--- src/main/java/org/example/jupjupticketserverapi/payment/repository/PaymentRepository.java
+++ src/main/java/org/example/jupjupticketserverapi/payment/repository/PaymentRepository.java
@@ -3,5 +3,9 @@ package org.example.jupjupticketserverapi.payment.repository;
 import org.example.jupjupticketserverapi.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByReservationId(Long reservationId);
 }
